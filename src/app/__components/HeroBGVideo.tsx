26446a1c45849394ab187f7470f9307446c72451
KentLouisArizo <kentlouisarizo.202100202@gmail.com>
2024-01-12 02:11:00 +0800

    fu

--- src/app/__components/HeroBGVideo.tsx
+++ src/app/__components/HeroBGVideo.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function BGVideo() {
   return (
-    <div className="absolute top-2 left-0">
+    <div className="absolute 4xl:top-5 4xl:right-0">
       <video
-        className=""
+        className="h-screenxl w-full "
         src={require("../__videos/TRIA.mp4")}
         autoPlay
         muted
